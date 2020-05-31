837d45a052a129f55f6849956f4eb19006782cc6
Ahmed ElMallah <ahmed.elmalah@live.com>
2020-05-31 04:13:05 -0400

    "Implemented the directive to use Intersection Observer API"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ng-in-viewport';
+  numbers: number[];
+
+  constructor() {
+    this.numbers = Array(25).fill(0).map((x,i)=>i);
+  }
+
+  ngOnInit() {
+    console.log(this.numbers);
+  }
+
+  onIntersection({target}: { target: ElementRef }) {
+    console.log(target);
+    target.nativeElement.classList.add("active");
+  }
 }
--- src/app/directives/in-viewport.directive.ts
+++ src/app/directives/in-viewport.directive.ts
@@ -7,10 +7,31 @@ export class InViewportDirective {
 
   @Output() public appInViewport: EventEmitter<any> = new EventEmitter();
   @Input() public inViewportOptions: string;
-  private _intersectionObserver? : IntersectionObserver;
+  private _intersectionObserver?: IntersectionObserver;
 
-  constructor (
+  constructor(
     private _element: ElementRef
   ) {}
 
+  public ngAfterViewInit() {
+    console.log(this.inViewportOptions);
+    this._intersectionObserver = new IntersectionObserver(entries => {
+      this.checkForIntersection(entries);
+    }, JSON.parse(this.inViewportOptions));
+    this._intersectionObserver.observe((this._element.nativeElement) as Element);
+  }
+
+  private checkForIntersection = (entries: Array<IntersectionObserverEntry>) => {
+    entries.forEach((entry: IntersectionObserverEntry) => {
+      if (this.checkIfIntersecting(entry)) {
+        this.appInViewport.emit({target: this._element});
+        this._intersectionObserver.unobserve((this._element.nativeElement) as Element);
+        this._intersectionObserver.disconnect();
+      }
+    });
+  }
+
+  private checkIfIntersecting(entry: IntersectionObserverEntry) {
+    return (entry as any).isIntersecting && entry.target === this._element.nativeElement;
+  }
 }
